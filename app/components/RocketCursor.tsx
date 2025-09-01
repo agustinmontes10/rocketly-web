2a7eb736972d18c44d30797f6a0da3bd9bbb37cb
unknown <jpcuadrelli@gmail.com>
2025-09-01 11:14:13 -0300

    some blems

--- app/components/RocketCursor.tsx
+++ app/components/RocketCursor.tsx
@@ -72,8 +72,10 @@ export default function RocketCursor() {
       const target = e.target as HTMLElement;
       if (target.closest('.navbar__link') || target.closest('.button')) {
         console.log('paso')
+        rocket.style.filter = 'brightness(3)';
+
         // rocket.style.filter = 'brightness(0) invert(1)';
-        rocket.style.filter = 'hue-rotate(180deg) brightness(1.2)';
+        // rocket.style.filter = 'hue-rotate(180deg) brightness(1.2)';
       } else {
         console.log('no paso')
         rocket.style.filter = 'none';
